f4940fec52052391daed2c960988bc321c2eb8dc
Hoan <nguyenanhhoanld@gmail.com>
2016-12-19 17:59:24 +0700

    Upload database script.

--- Database/web2.sql
+++ Database/web2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2016 at 03:38 AM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
+-- Generation Time: Dec 19, 2016 at 10:58 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,12 +29,11 @@ USE `web2`;
 --
 
 DROP TABLE IF EXISTS `banners`;
-CREATE TABLE IF NOT EXISTS `banners` (
+CREATE TABLE `banners` (
   `banner_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `banner_title` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `banner_img` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `banner_type` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`banner_id`)
+  `banner_type` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -42,10 +41,11 @@ CREATE TABLE IF NOT EXISTS `banners` (
 --
 
 INSERT INTO `banners` (`banner_id`, `banner_title`, `banner_img`, `banner_type`) VALUES
-('B01', 'Sky1', 'images/banners/57977.jpg', 'home'),
+('B01', 'Sky1', 'images/banners/62055.jpg', 'home'),
 ('B02', 'Sky2', 'images/banners/12809.jpg', 'home'),
 ('B03', 'Sky3', 'images/banners/93584.jpg', 'home'),
-('B04', 'Sky4', 'images/banners/70686.jpg', 'home');
+('B04', 'Sky4', 'images/banners/70686.jpg', 'home'),
+('B05', 'SS', 'images/banners/23415.jpg', 'home');
 
 -- --------------------------------------------------------
 
@@ -54,13 +54,12 @@ INSERT INTO `banners` (`banner_id`, `banner_title`, `banner_img`, `banner_type`)
 --
 
 DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
+CREATE TABLE `categories` (
   `category_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `category_title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `category_description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `category_type` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
-  `category_image` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`category_id`)
+  `category_image` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -68,9 +67,10 @@ CREATE TABLE IF NOT EXISTS `categories` (
 --
 
 INSERT INTO `categories` (`category_id`, `category_title`, `category_description`, `category_type`, `category_image`) VALUES
-('C01', 'Rooms Listing', 'Salutantibus vitae elit libero, a pharetra augue. At nos hinc posthac, sitientis piros Afros. Quisque ut dolor gravida, placerat libero vel, euismod. Ut enim ad minim veniam, quis nostrud exercitation. Unam incolunt Belgae, aliam Aquitani, tertiam. Unam incolunt Belgae, aliam Aquitani, tertiam. Tityre, tu patulae recubans sub tegmine fagi dolor. Curabitur est gravida.', 'Rooms Listing', 'images/categories/25832.jpg'),
-('C02', 'Pro Golf Club', 'Ut enim ad minim veniam, quis nostrud exercitation. Fabio vel iudice vincam, sunt in culpa qui officia.', 'Pro Golf Club', 'images/categories/35215.jpg'),
-('C03', 'Swimming Pools', 'Salutantibus vitae elit libero, a pharetra augue. At nos hinc posthac, sitientis piros Afros. Quisque ut dolor gravida, placerat libero vel, euismod. Ut enim ad minim veniam, quis nostrud exercitation. Unam incolunt Belgae, aliam Aquitani, tertiam. Unam incolunt Belgae, aliam Aquitani, tertiam. Tityre, tu patulae recubans sub tegmine fagi dolor. Curabitur est gravida.', 'Swimming Pools', 'images/categories/85913.jpg');
+('C01', 'Rooms Listing', 'The exceptional accommodation at our eco resort includes 25 superbly appointed Eco Villas interlinked by over 1km of elevated wooden boardwalks, and 30 luxurious safari style Eco Tents. For larger groups we also offer The Beach Houses, striking ocean front accommodation with amazing views of the Indian Ocean.', 'Rooms Listing', 'images/categories/25832.jpg'),
+('C04', 'SS', '', 'Tennis Courts', 'images/categories/55194.jpg'),
+('C02', 'Pro Golf Club', 'Occupying arguably the finest position of any restaurant in Western Australia, Jack’s Bar & Restaurant provides the perfect setting for dining and relaxation whilst indulging in the stunning ocean views and mouth-watering cuisine.', 'Pro Golf Club', 'images/categories/35215.jpg'),
+('C03', 'Swimming Pools', 'Get out of Broome for the day and come fish Eco Beach. The fishing is superb, regularly landing a dozen different species a session. From Barra up the creek to Billfish along the coast, Broome Sportfishing has it covered!', 'Swimming Pools', 'images/categories/85913.jpg');
 
 -- --------------------------------------------------------
 
@@ -79,14 +79,13 @@ INSERT INTO `categories` (`category_id`, `category_title`, `category_description
 --
 
 DROP TABLE IF EXISTS `details`;
-CREATE TABLE IF NOT EXISTS `details` (
+CREATE TABLE `details` (
   `detail_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `detail_title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `detail_brief_description` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `detail_full_description` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
   `detail_image` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `detail_type` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`detail_id`)
+  `detail_type` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -114,12 +113,11 @@ INSERT INTO `details` (`detail_id`, `detail_title`, `detail_brief_description`, 
 --
 
 DROP TABLE IF EXISTS `features`;
-CREATE TABLE IF NOT EXISTS `features` (
+CREATE TABLE `features` (
   `feature_id` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `feature_title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `feature_type` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `feature_content` varchar(100) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`feature_id`)
+  `feature_content` varchar(100) CHARACTER SET utf8 NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -142,12 +140,11 @@ INSERT INTO `features` (`feature_id`, `feature_title`, `feature_type`, `feature_
 --
 
 DROP TABLE IF EXISTS `menus`;
-CREATE TABLE IF NOT EXISTS `menus` (
+CREATE TABLE `menus` (
   `menu_id` varchar(10) NOT NULL,
   `menu_title` varchar(50) NOT NULL,
   `menu_level` int(2) NOT NULL,
-  `menu_parents` varchar(10) NOT NULL,
-  PRIMARY KEY (`menu_id`)
+  `menu_parents` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -158,7 +155,8 @@ INSERT INTO `menus` (`menu_id`, `menu_title`, `menu_level`, `menu_parents`) VALU
 ('M01', 'Rooms Listing', 1, ''),
 ('M02', 'Pro Golf Club', 1, ''),
 ('M03', 'Swimming Pools', 1, ''),
-('M04', 'Tennis Courts', 1, '');
+('M04', 'Tennis Courts', 1, ''),
+('M05', 'Gold', 1, '');
 
 -- --------------------------------------------------------
 
@@ -167,12 +165,11 @@ INSERT INTO `menus` (`menu_id`, `menu_title`, `menu_level`, `menu_parents`) VALU
 --
 
 DROP TABLE IF EXISTS `prices`;
-CREATE TABLE IF NOT EXISTS `prices` (
+CREATE TABLE `prices` (
   `price_id` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
   `price_rate` int(3) NOT NULL,
   `price_type` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `price_detail` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`price_id`)
+  `price_detail` varchar(30) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -200,16 +197,15 @@ INSERT INTO `prices` (`price_id`, `price_rate`, `price_type`, `price_detail`) VA
 --
 
 DROP TABLE IF EXISTS `reservations`;
-CREATE TABLE IF NOT EXISTS `reservations` (
-  `reservation_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reservations` (
+  `reservation_id` int(11) NOT NULL,
   `reservation_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `reservation_email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `reservation_arrival_date` date NOT NULL,
   `reservation_departure_date` date NOT NULL,
   `reservation_type` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `reservation_number_people` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`reservation_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `reservation_number_people` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `reservations`
@@ -217,8 +213,63 @@ CREATE TABLE IF NOT EXISTS `reservations` (
 
 INSERT INTO `reservations` (`reservation_id`, `reservation_name`, `reservation_email`, `reservation_arrival_date`, `reservation_departure_date`, `reservation_type`, `reservation_number_people`) VALUES
 (5, 'Grim', 'grimreaperld@gmail.com', '2016-12-25', '2016-12-30', 'Round Cove Suite', '4 Adults'),
-(6, '123', '10000714757654', '2016-12-12', '2016-12-12', 'Round Cove Suite', '1 Adult');
+(19, 'Truc', 'sontung.buinguyen@gmail.com', '2016-12-31', '2016-12-31', 'Round Cove Suite', '2 Adults');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`banner_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `details`
+--
+ALTER TABLE `details`
+  ADD PRIMARY KEY (`detail_id`);
+
+--
+-- Indexes for table `features`
+--
+ALTER TABLE `features`
+  ADD PRIMARY KEY (`feature_id`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`menu_id`);
+
+--
+-- Indexes for table `prices`
+--
+ALTER TABLE `prices`
+  ADD PRIMARY KEY (`price_id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`reservation_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
